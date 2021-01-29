b9fbaf91ecb22de0fd67ac0e63bd589cc94885e8
Alexey Perevalov <alexey.perevalov@huawei.com>
2021-01-29 06:57:24 +0000

    TopologyManager Scope support
    This policy accounts resources per pod level basis.
    
    Signed-off-by: Alexey Perevalov <alexey.perevalov@huawei.com>

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -49,7 +49,7 @@ func main() {
 		app.WithPlugin(loadvariationriskbalancing.Name, loadvariationriskbalancing.New),
 		app.WithPlugin(noderesources.AllocatableName, noderesources.NewAllocatable),
 		app.WithPlugin(targetloadpacking.Name, targetloadpacking.New),
-		app.WithPlugin(noderesourcetopology.NodeResourceTopologyMatchName, noderesourcetopology.NewNodeResourceTopologyMatch),
+		app.WithPlugin(noderesourcetopology.Name, noderesourcetopology.New),
 		// Sample plugins below.
 		app.WithPlugin(crossnodepreemption.Name, crossnodepreemption.New),
 		app.WithPlugin(podstate.Name, podstate.New),
